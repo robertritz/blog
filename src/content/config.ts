2978863e743215367dafcaabcb0ea8b0acb701f9
Robert Ritz <robertritz@outlook.com>
2024-11-20 21:41:27 +0800

    Initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+
+const posts = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    image: z.string().default("/blog-placeholder.svg"),
+  }),
+});
+
+export const collections = { posts };
